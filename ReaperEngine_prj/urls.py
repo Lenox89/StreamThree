5e1d112a0dd979fdb35fcd62c29720b3bee7bd25
Lenox89 <cameron.macon@gmail.com>
2019-03-11 22:15:23 -0700

    Initial Commit

---
+++ ReaperEngine_prj/urls.py
@@ -0,0 +1,26 @@
+"""ReaperEngine_prj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from ReaperEngine_app import views as ReaperEngine_views
+from accounts_app import views as accounts_views
+from support_app import views as support_views
+from blog_app import views as blog_views
+from donations_app import views as donations_views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+]
