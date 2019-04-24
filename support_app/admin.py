ca056c9b921eb7ee7f7956770f390dd1d7fece6b
Lenox89 <cameron.macon@gmail.com>
2019-04-24 15:24:42 -0700

    updated admin panel

--- support_app/admin.py
+++ support_app/admin.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Ticket
 from django.contrib import admin
 
 # Register your models here.
+def make_todo(ModelAdmin,request, queryset):
+  queryset.update(status='Pending')
+make_todo.short_description = "Mark selected as pending"
+
+def make_doing(ModelAdmin,request, queryset):
+  queryset.update(status='Working')
+make_doing.short_description = "Mark selected as working"
+
+def make_done(ModelAdmin,request, queryset):
+  queryset.update(status='Complete')
+make_done.short_description = "Mark selected as complete"
+
+class Admin_Ticket(admin.ModelAdmin):
+  list_display = ['id','issue','type','status','user','modified']
+  list_editable = ['status','type']
+  date_hierarchy = 'modified'
+  # date_hierarchy= 'modified'
+  actions = [make_done,make_doing,make_todo]
+
+admin.site.register(Ticket,Admin_Ticket)
